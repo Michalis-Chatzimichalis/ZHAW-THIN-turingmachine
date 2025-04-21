fc4a469b7f754c2e1bdf0df136facf0b65f22fdc
Michalis-Chatzimichalis <apollon.michalis@gmail.com>
2025-04-21 16:18:03 +0200

    added main class

---
+++ Turingmachine.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class Turingmachine {
+
+    public Turingmachine() {
+        // Constructor
+    }
+
+    public Turingmachine(String input) {
+        // Constructor with input
+    }
+
+    public Turingmachine(File file) {
+        // Constructor with file
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
